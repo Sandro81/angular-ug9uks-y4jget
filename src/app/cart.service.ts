3293ff45567beb4d147a1e77d0dd1ab8c46d906b
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-01 07:14:01 +0100

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -7,4 +7,17 @@ import { Injectable } from '@angular/core';
 export class CartService {
   //In the CartService class, define an items property to store the list (array) of the current products in the cart.
   items = [];
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
